0190db0b9ce1aa7204098dd592041eae811dd477
Patryk Graczyk <patryk180@wp.pl>
2017-05-17 16:05:30 +0200

    pliki logowania

---
+++ pass_recover.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Odzyskiwanie hasla</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="css/login.css" />
+	</head>
+	<body>
+			
+					
+					<p>Zapomniałeś hasła?<p><br />
+					<a href="login.php">Zaloguj ponownie</a>
+					<a href="register.php">Nowe konto</a>
+			<form action="index.php">
+			
+				<p>Pomocniczy e-mail<input type="text" /></p><br /><br />
+			<input type="submit" value="Wyślij"/><br /><br />
+			</form>
+		
+	</body>
+</html>
---
+++ register.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>Odzyskiwanie hasla</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="css/login.css" />
+	</head>
+	<body>
+			
+					
+					<p>REJESTRACJA<p><br />
+					<a href="login.php">Logowanie</a>
+					
+			<form action="index.php">
+			
+				<p>login<input type="text" /></p>
+				<p>haslo<input type="text" /></p>
+			<input type="submit" value="Zarejestruj"/><br /><br />
+			</form>
+		
+	</body>
+</html>
