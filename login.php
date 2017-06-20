87fc3d0e26fad2ea2ab5beeb71f82f971f5e7a29
Patryk Graczyk <patryk180@wp.pl>
2017-06-20 10:30:56 +0200

    podlaczenie pliku connection.php

--- login.php
+++ login.php
@@ -1,22 +1,33 @@
-﻿<!DOCTYPE HTML>
-<html>
+﻿
+
 	<head>
 		<title>Lodówka - logowanie</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="css/login.css" />
+	
 	</head>
 	<body>
 		<div id="container">
+			<div id="naglowek">
+			
 			<form action="index.php">
 				<p style="text-align:center;margin:0;">Witaj w programie <strong>Lodówka!</strong></p>
 				<p style="text-align:center;margin:0;">Zaloguj się, aby rozpocząć</p><br /><br />
 				<label>Login:<input type="text" /></label><br /><br />
 				<label>Hasło: <input type="password" /></label><br /><br />
 				<input type="submit" value="Zaloguj" /><br /><br />
-				<a href="#">Zapomniałeś hasła?</a><br />
-				<a href="#">Zarejestruj się</a>
+				<a href="pass_recover.php">Zapomniałeś hasła?</a><br />
+				<a href="register.php">Zarejestruj się</a></div>
+				
 			</form>
 		</div>
 	</body>
-</html>
+</html
+<?php
+	include "connection.php";
+	connection();
+?>
+
+
+
